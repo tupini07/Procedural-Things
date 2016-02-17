980fac3eaa0010437e8040f11d6ef05afdfff8b6
Andrea Tupini <tupini07@gmail.com>
2016-02-17 16:04:01 -0600

    added sample proc image and loading

---
+++ ProceduralFunctions/simpleSpiral.js
@@ -0,0 +1,9 @@
+procFunc.simpleSpiral = function(){
+  ctx.moveTo(0,0);
+  ctx.lineTo(200,100);
+
+  ctx.fillStyle = "red";
+  ctx.moveTo(10,0);
+  ctx.lineTo(100,500);
+  ctx.stroke();
+};
--- proc.js
+++ proc.js
@@ -1,5 +1,11 @@
-angular.module('procApp', [])
-  .controller('ProceduralController', function() {
-    var proc = this;
+var procFunc = {}; //global object to hold all procedural functions
+var mCanvas = document.getElementById("mCanvas"); //the canvas element
+var ctx = mCanvas.getContext("2d"); //2Dimensional global context
 
-  });
+angular.module('procApp', []).controller('ProceduralController', function() {
+    var proc = this;
+    window.onload = function(){
+      procFunc.simpleSpiral();
+    };
+    
+});
